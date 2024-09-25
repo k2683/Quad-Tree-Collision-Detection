fce2cddaef04a853d3e2e91b9265f6dbe4cffe84
Benhou Li <benhou@kth.se>
2024-09-26 00:43:02 +0200

    recommit

---
+++ Assets/OPH/Collision/QuadTree/IRect.cs
@@ -0,0 +1,23 @@
+namespace OPH.Collision.QuadTree {
+    /// <summary>
+    /// 功能说明：范围接口
+    /// </summary>
+    public interface IRect {
+        /// <summary>
+        /// 矩形中心X
+        /// </summary>
+        float X { get; set; }
+        /// <summary>
+        /// 矩形中心Y
+        /// </summary>
+        float Y { get; set; }
+        /// <summary>
+        /// 矩形宽
+        /// </summary>
+        float Width { get; set; }
+        /// <summary>
+        /// 矩形高
+        /// </summary>
+        float Height { get; set; }
+    }
+}
